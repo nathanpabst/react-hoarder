59dbb323468d707a384ab96f3d4731417f7f3108
Nathan Pabst <nathan.pabst@gmail.com>
2018-07-11 20:42:21 -0500

    routing in place

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,115 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {Route, BrowserRouter, Redirect, Switch} from 'react-router-dom';
+import firebase from 'firebase';
+
 import './App.css';
 
-class App extends Component {
+import Navbar from '../components/Navbar/Navbar';
+import Login from '../components/Login/Login';
+import Register from '../components/Register/Register';
+import Home from '../components/Home/Home';
+import AllTheThings from '../components/AllTheThings/AllThings';
+import MyThings from '../components/MyThings/MyThings';
+import fbConnection from '../firebaseRequests/connection';
+fbConnection();
+
+const PrivateRoute = ({ component: Component, authed, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/login', state: {from: props.location}}}
+          />
+        )
+      }
+    />
+  );
+};
+
+const PublicRoute = ({ component: Component, authed, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === false ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/orders', state: {from: props.location}}}
+          />
+        )
+      }
+    />
+  );
+};
+
+class App extends React.Component {
+  state={
+    authed: false,
+  }
+
+  componentDidMount () {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({authed: true});
+      } else {
+        this.setState({authed: false});
+      };
+    });
+  }
+
+  componentWillUnmount () {
+    this.removeListener();
+  }
+
+  runAway = () => {
+    this.setState({authed: false});
+  }
+
   render () {
     return (
       <div className="App">
-      <button className="btn btn-danger">imaButton</button>
-      </div>
+        <BrowserRouter>
+          <div>
+            <Navbar
+              authed={this.state.authed}
+              runAway={this.runAway}
+            />
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <Route path="/" exact component={Home}/>
+                  <PrivateRoute
+                    path="/myStuff/:id"
+                    authed={this.state.authed}
+                    component={MyThings}
+                  />
+                  <PublicRoute
+                    path="/register"
+                    authed={this.state.authed}
+                    component={Register}
+                  />
+                  <PublicRoute
+                    path="/Login"
+                    authed={this.state.authed}
+                    component={Login}
+                  />
 
+                  <PrivateRoute
+                    path="/allTheThings"
+                    authed={this.state.authed}
+                    component={AllTheThings}
+                  />
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </BrowserRouter>
+      </div>
     );
   }
 }
